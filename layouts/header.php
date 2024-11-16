614ac4568884d23237533432d3700b4366249a23
EduCostam <eduardomc.devs@gmail.com>
2024-11-15 23:52:58 -0300

    first commit - header

---
+++ layouts/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, inicial-scale=1.0">
+    <title>Consulta de Signos</title>
+    <link href="https://cdn.jsdlivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>   
